b4b419d75b4acda898254e60dedcb1c7a28b2c4d
joaonpsilva <joao.npsilva@ua.pt>
2021-05-29 17:24:09 +0100

    rename and delete package-infos

--- PA3_GX/src/Client/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* This package contains the classes and methods to play the Client role.
-* Please read the requirements carefully.
-* 
-**/
-
-package Client;
--- PA3_GX/src/LoadBalancer/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* This package contains the classes and methods to play the Load Balancer role. 
-* Please read the requirements carefully.
-* 
-**/
-
-package LoadBalancer;
